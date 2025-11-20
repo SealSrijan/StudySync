78dd355af05f1638eda0e0a50f384aaa889c4b67
SealSrijan <creative.ideas1610@gmail.com>
2025-11-20 10:55:31 +0530

    chore: commit and push workspace changes

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAsBS6lKFlEMUv7OW-jgwVNj9dDmWJ3Rd8",
   authDomain: "studysync-e338a.firebaseapp.com",
   projectId: "studysync-e338a",
-  storageBucket: "studysync-e338a.firebasestorage.app",
+  storageBucket: "studysync-e338a.appspot.com",
   messagingSenderId: "799347574826",
   appId: "1:799347574826:web:8ec5bc6f9f7759d9eb9b53"
 };
